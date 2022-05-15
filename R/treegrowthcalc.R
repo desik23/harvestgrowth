2102beb815baa21553d8af60ae0134af1827d284
Desik Somasundaram <88209511+desik23@users.noreply.github.com>
2022-05-15 14:25:43 -0700

    boxplots and ode solver sobal

--- R/treegrowthcalc.R
+++ R/treegrowthcalc.R
@@ -1,24 +1,24 @@
 #'  More complex population growth
 #' @param T  period of growth
-#' @param C size of forest
-#' @param thresh canopy cover threshold
+#' @param Cinitial size of forest
+#' @param parms$thresh canopy cover threshold
 #' @param parms$r - exp growth rate
 #' @param parms$g - linear growth rate
 #' @param parms$K - carrying capacity
 #' @return change in population
 #'
-treegrowthcalc = function(time, C, thresh, parms) {
-  if(C > thresh){
+treegrowthcalc = function(time, Cinitial, parms) {
+  if (Cinitial > parms$thresh) {
     # compute rate of change of population using exp growth rate
-    treegrowth = parms$r*C
-  } else (C <= thresh){
+    treegrowth = parms$r*Cinitial
+  } else if(Cinitial <= parms$thresh){
     # compute rate of change of population using linear growth rate
-    treegrowth = parms$g*(1 - C/K)
+    treegrowth = parms$g*(1 - Cinitial/K)
   }
     
 
 
   # set rate of change to 0 if P is greater than carrying capacity
-  treegrowth = ifelse(C > parms$K, 0, treegrowth)
+  treegrowth = ifelse(Cinitial > parms$K, 0, treegrowth)
   return(list(treegrowth))
 }
